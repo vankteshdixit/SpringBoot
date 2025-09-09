79e2a3e43b729af2edaa1ce1db38fd9b40514de6
Vanktesh <dixitvanktesh2003@gmail.com>
2025-09-09 09:31:12 +0530

    define employeeController for a controller layer

---
+++ src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -0,0 +1,10 @@
+package com.example.demo.controllers;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RestController -> it will handle HTTP request
+public class EmployeeController {
+    
+}

eca5268a02c8b1da3ed64f4a02ffad957cacaae9
Vanktesh <dixitvanktesh2003@gmail.com>
2025-09-09 11:14:15 +0530

    make a parent->@RequestMapping and children that append the url path of a @RequestMapping

--- src/main/java/com/example/demo/controllers/EmployeeController.java
+++ src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -2,24 +2,21 @@ package com.example.demo.controllers;
 
 
 import com.example.demo.dto.EmployeeDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 //@RestController -> it will handle HTTP request]
-
+@RequestMapping(path = "/employee")
 public class EmployeeController {
-    @GetMapping(path = "/employees/{employeeID}")
+    @GetMapping(path = "/{employeeID}")
     public EmployeeDTO getEmployeeById(@PathVariable Long employeeID){
         return new EmployeeDTO(employeeID, "Vanktesh", "dixitvanktesh2003@gmail.com", 22, LocalDate.of(2025, 1, 2),true);
     }
 
-    @GetMapping(path = "/employees")
+    @GetMapping
     public String getAllEmployees(@RequestParam Integer age){
         return "Hi age "+age;
     }

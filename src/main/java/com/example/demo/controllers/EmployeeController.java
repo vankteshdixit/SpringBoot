f632ad37ae685edc626dc8f7697072c34562a091
Vanktesh <dixitvanktesh2003@gmail.com>
2025-09-09 11:12:10 +0530

    do some work with the @PathVariable and @RequestMapping -> these are independent

--- src/main/java/com/example/demo/controllers/EmployeeController.java
+++ src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -1,10 +1,26 @@
 package com.example.demo.controllers;
 
 
+import com.example.demo.dto.EmployeeDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @RestController
-//@RestController -> it will handle HTTP request
+//@RestController -> it will handle HTTP request]
+
 public class EmployeeController {
-    
+    @GetMapping(path = "/employees/{employeeID}")
+    public EmployeeDTO getEmployeeById(@PathVariable Long employeeID){
+        return new EmployeeDTO(employeeID, "Vanktesh", "dixitvanktesh2003@gmail.com", 22, LocalDate.of(2025, 1, 2),true);
+    }
+
+    @GetMapping(path = "/employees")
+    public String getAllEmployees(@RequestParam Integer age){
+        return "Hi age "+age;
+    }
 }
